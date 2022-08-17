8f6397e531dfe7dbe751315719b8490d4b40bc62
Fiona Snyder <fiona.snyder@outlook.com>
2022-08-17 10:52:32 -0500

    mock CC

--- src/index.js
+++ src/index.js
@@ -1,4 +1,50 @@
 // write your code here
+
 function fetchResource() {
-    console.log()
+    fetch ("http://localhost:3000/ramens")
+    .then (res => res.json)
+    .then (data => data.forEach(ramen => renderARamen(ramen)))
 }
+
+function renderARamen(ramen) {
+    const container = document.querySelector("#ramen-menu")
+
+    let card = document.createElement('div')
+    card.className = "ramen-detail"
+    
+    let image = document.querySelector('.detail-image')
+    card.append(image)
+    image.src = ramen["image"]
+
+    container.append(card)
+}
+
+//renderARamen(ramen)
+fetchResource()
+
+function ramenForm() {
+    const form = document.querySelector("#new-ramen")
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        const name = e.target["name"].value
+        const restaurant = e.target["restaurant"].value
+        const image = e.target["image"].value
+        const rating = e.target["rating"].value
+        const comment = e.target["new-comment"].value
+
+        const newRamen = {
+            name: name,
+            restaurant: restaurant,
+            image: image,
+            rating: rating,
+            comment: []
+        }
+
+        renderARamen(newRamen)
+    })
+}
+
+ramenForm()
+
+
